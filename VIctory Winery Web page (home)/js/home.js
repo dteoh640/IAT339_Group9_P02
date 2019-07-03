4aa970cedfc0f265b85e6ed23a31dda94fa79f55
bobo <liyunbo11123@gmail.com>
2019-07-02 17:14:20 -0700

    optimize on navigation bar, and add event detailed page

--- VIctory Winery Web page (home)/js/home.js
+++
@@ -1,7 +0,0 @@
-  $(window).scroll(function () {
-        if ($(window).scrollTop() > 80) {
-            $(".navbar-fixed-top").addClass("bg-nav");
-        } else {
-            $(".navbar-fixed-top").removeClass("bg-nav");
-        }
-    });
